4bb7851637c987480e64adc9b3a331e4d86a5caf
Andy Newton <andy@arin.net>
2015-02-06 16:56:19 -0500

    added some code for submitting the fields

---
+++ src/site.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015. American Registry for Internet Numbers
+ */
+
+/**
+ *
+ * Created by anewton on 2/6/15.
+ */
+
+/*
+ * makes the #queryterm div and inserts it after #querycontainer
+ * returns the #queryterm div node
+ */
+function makeQueryTermDiv( term, description ) {
+  var $queryTerm = $('<div class="container" id="queryterm"/>' );
+  $queryTerm.append( $('<p>Query term is "' + term + '".</p>') );
+  $queryTerm.append( $('<p>Query is assumed to be ' + description + '.</p>') );
+  $('<div class="progress" />' )
+    .append( $('<div class="progress-bar progress-bar-striped active" />')
+                  .attr("role","progressbar")
+                  .attr("aria-valuenow","100")
+                  .attr("aria-valuemin","0")
+                  .attr("aria-valuemax","100")
+                  .attr("style","width:100%"))
+          .appendTo( $queryTerm );
+  $('#querycontainer' ).after( $queryTerm );
+  return $queryTerm;
+}
+
+function clearDivs()
+{
+  $( '#querycontainer ~ div' ).remove();
+}
+
+$(document).ready( function() {
+
+  $('#queryform').submit( function() {
+    clearDivs();
+    makeQueryTermDiv( "192.133.133.4", "an IPv4 address" )
+    return false;
+  } );
+
+  $('#clear' ).click( function() {
+    clearDivs();
+  });
+});
