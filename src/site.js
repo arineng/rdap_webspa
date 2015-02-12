e6fcd561b31d1e60025f94a71608e27b1f99cebb
Andy Newton <andy@arin.net>
2015-02-12 10:57:29 -0500

    prototyping use of treeview

--- src/site.js
+++ src/site.js
@@ -369,6 +369,75 @@ $(document).ready( function() {
           [ [ "Name", "Andy Newton" ], [ "Email", "andy@arin.net" ], [ "Handle", "ALN-ARIN" ] ],
           "Results truncated") );
 
+  var defaultData = [
+    {
+      text: 'Parent 1',
+      href: '#parent1',
+      tags: ['4'],
+      nodes: [
+        {
+          text: 'Child 1',
+          href: '#child1',
+          tags: ['2'],
+          nodes: [
+            {
+              text: 'Grandchild 1',
+              href: '#grandchild1',
+              tags: ['0']
+            },
+            {
+              text: 'Grandchild 2',
+              href: '#grandchild2',
+              tags: ['0']
+            }
+          ]
+        },
+        {
+          text: 'Child 2',
+          href: '#child2',
+          tags: ['0']
+        }
+      ]
+    },
+    {
+      text: 'Parent 2',
+      href: '#parent2',
+      tags: ['0']
+    },
+    {
+      text: 'Parent 3',
+      href: '#parent3',
+      tags: ['0']
+    },
+    {
+      text: 'Parent 4',
+      href: '#parent4',
+      tags: ['0']
+    },
+    {
+      text: 'Parent 5',
+      href: '#parent5'  ,
+      tags: ['0']
+    }
+  ];
+
+  //test of the method... remove later
+  $('#treeview').treeview({
+                             color: "#428bca",
+                             expandIcon: "glyphicon glyphicon-stop",
+                             collapseIcon: "glyphicon glyphicon-unchecked",
+                             nodeIcon: "glyphicon glyphicon-user",
+                             showTags: true,
+                             data: defaultData
+                           });
+
+
+  //test of the method... remove later
+  $('#treeview' ).on('nodeSelected', function() {
+    $('#dataview' ).append( makeOCTable( "Entity", "ALN-ARIN",
+                                         [ [ "Name", "Andy Newton" ], [ "Email", "andy@arin.net" ], [ "Handle", "ALN-ARIN" ] ] ) );
+  });
+
   //test of the method... remove later
   $('#results' ).append( makeOCTable( "Entity", "ALN-ARIN",
     [ [ "Name", "Andy Newton" ], [ "Email", "andy@arin.net" ], [ "Handle", "ALN-ARIN" ] ] ) );
